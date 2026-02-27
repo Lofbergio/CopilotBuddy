acbbc67fc4c8fb80ca97346474cbc0346672e00b
Likon69 <Andrei69330@gmail.com>
2026-02-27 23:54:42 +0100

    add obsolete INavigationProvider interface

---
+++ Styx/Logic/Pathing/INavigationProvider.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Styx.Logic.Pathing
+{
+    [Obsolete("If deriving from this class, derive from NavigationProvider instead. If using this class, use wrapper functions in Navigator instead.")]
+    public interface INavigationProvider
+    {
+        MoveResult MoveTo(WoWPoint location);
+
+        float PathPrecision { get; set; }
+
+        bool Clear();
+
+        WoWPoint[] GeneratePath(WoWPoint from, WoWPoint to);
+
+        bool CanNavigateFully(WoWPoint from, WoWPoint to, int maxHops);
+
+        IStuckHandler StuckHandler { get; set; }
+
+        bool AtLocation(WoWPoint point1, WoWPoint point2);
+    }
+}
