941017efec44ffe826f8460cd68384baa3904916
Likon69 <Andrei69330@gmail.com>
2026-03-05 18:20:54 +0100

    Fix ForcedSingleton to match HB 4.3.4

--- Bots/Quest/QuestOrder/ForcedSingleton.cs
+++ Bots/Quest/QuestOrder/ForcedSingleton.cs
@@ -1,34 +1,42 @@
 // ForcedSingleton.cs - A behavior that executes a single action
 // Ported from HB 4.3.4
 
+using CommonBehaviors.Actions;
 using TreeSharp;
 
 namespace Bots.Quest.QuestOrder
 {
     /// <summary>
-    /// A forced behavior that executes a single action and completes.
+    /// A forced behavior that executes a single action and completes immediately.
     /// Used for simple one-shot actions like setting grind area, vendors, etc.
+    /// HB 4.3.4: Action runs in OnStart(), IsDone is always true, Branch is ActionAlwaysSucceed.
+    /// This lets the while(IsDone) loop in ForcedBehaviorExecutor process singletons
+    /// instantly without consuming a full tick.
     /// </summary>
     public class ForcedSingleton : ForcedBehavior
     {
         private readonly System.Action _action;
-        private bool _done;
 
         public ForcedSingleton(System.Action action)
         {
             _action = action ?? throw new System.ArgumentNullException(nameof(action));
         }
 
-        public override bool IsDone => _done;
+        public override bool IsDone => true;
+
+        public override void OnStart()
+        {
+            _action();
+        }
 
         protected override Composite CreateBehavior()
         {
-            return new TreeSharp.Action(ctx =>
-            {
-                _action();
-                _done = true;
-                return RunStatus.Success;
-            });
+            return new ActionAlwaysSucceed();
+        }
+
+        public override string ToString()
+        {
+            return "[ForcedSingleton]";
         }
     }
 }
