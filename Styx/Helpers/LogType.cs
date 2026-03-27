55e370dd96af299b77aba5bf51fb156bbf3bda4e
Likon69 <Andrei69330@gmail.com>
2026-03-27 19:06:10 +0100

    Add Styx/Helpers/LogType.cs - log type enum

---
+++ Styx/Helpers/LogType.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Styx.Helpers
+{
+    /// <summary>
+    /// Log type for the logging system.
+    /// Ported from HB 4.3.4.
+    /// </summary>
+    public enum LogType
+    {
+        Normal,
+        Debug,
+        Navigator,
+        FileOnly
+    }
+}
